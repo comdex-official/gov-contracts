623cb9f689e8b3c72100206bf3ccd328b97e7ca2
Subham <39151404+Subham2804@users.noreply.github.com>
2022-06-29 04:14:48 +0530

    arm 64 wasm optimizer

---
+++ scripts/check_workspace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+cargo fmt
+(cd packages/bindings && cargo check && cargo clippy --all-targets -- -D warnings)
+(cd packages/cw3 && cargo check && cargo clippy --all-targets -- -D warnings)
+(cd packages/cw4 && cargo check && cargo clippy --all-targets -- -D warnings)
+(
+  cd packages/std
+  cargo check
+  cargo check --features iterator,staking,stargate
+  cargo wasm-debug
+  cargo wasm-debug --features iterator,staking,stargate
+  cargo clippy --all-targets --features iterator,staking,stargate -- -D warnings
+  cargo schema
+)
+(cd packages/storage && cargo build && cargo clippy --all-targets --features iterator -- -D warnings)
+(cd packages/schema && cargo build && cargo clippy --all-targets -- -D warnings)
+(cd packages/vm && cargo build --features iterator,stargate && cargo clippy --all-targets --features iterator,stargate -- -D warnings)
