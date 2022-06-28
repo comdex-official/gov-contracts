c5b8f8af4168f299f878766224b0cf44e41fb832
Naveen <naveen@Naveens-MacBook-Air.local>
2022-06-28 13:04:53 +0530

    Updated testcase

--- contracts/governance/src/integration_test.rs
+++ contracts/governance/src/integration_test.rs
@@ -1,5 +1,5 @@
 use comdex_bindings::ComdexMessages;
-use cosmwasm_std::{testing::{mock_info, mock_env}, Decimal, Uint128, Coin, Response, Addr};
+use cosmwasm_std::{testing::{mock_info, mock_env}, Decimal, Uint128, Coin, Response, Addr, BankMsg};
 use cw3::{Status, Vote};
 use cw_storage_plus::Map;
 use cw_utils::{Threshold, Expiration, Duration};
@@ -35,8 +35,10 @@ mod tests {
 #[test]
     fn test_gov() {
         tests::mock_dependencies1();
-        let mut deps = tests::mock_dependencies1();
         const OWNER: &str = "admin001";
+        let addr1 = Addr::unchecked("addr1");
+        let addr2 = Addr::unchecked("addr2");
+        let mut deps = tests::mock_dependencies1();
         let info = mock_info(OWNER, &[]);
 
         let instantiate_msg = InstantiateMsg {
@@ -56,7 +58,7 @@ mod tests {
             description: "test prop".to_string(),
             start_height: 43,
             expires: Expiration::AtTime(cosmwasm_std::Timestamp::from_nanos(1_655_745_430)),
-            msgs: vec![ComdexMessages::MsgWhitelistAppIdVaultInterest { app_mapping_id: 33 }],
+            msgs: vec![ComdexMessages::MsgWhitelistAppIdVaultInterest { app_mapping_id: id }],
             status: Status::Open,
             duration: Duration::Time(40),
             threshold: Threshold::ThresholdQuorum {
@@ -78,18 +80,30 @@ mod tests {
             token_denom: "toVote".to_string(),
             min_deposit: 45,
             current_deposit: 56,
-            app_mapping_id: 33,
+            app_mapping_id: id,
             is_slashed: true,
         };
         prop.update_status(&mock_env().block);
         PROPOSALS.save(&mut deps.storage, id, &prop);
+        pub const VOTERDEPOSIT: Map<(u64, &Addr), Vec<Coin>> = Map::new("voter deposit");
+        let info = mock_info(OWNER, &[]);
+        let mut _vote = VOTERDEPOSIT.save(&mut deps.storage, (id, &info.sender), &info.funds);
+        
+        let _deposit_info = VOTERDEPOSIT
+            .may_load(&deps.storage, (id, &info.sender))
+            .unwrap();
 
+        let deposit_msg = ExecuteMsg::Deposit { 
+            proposal_id: id 
+        };
+        execute(deps.as_mut(), mock_env(), info.clone(),deposit_msg);
+        // let err = execute_deposit(deps.as_mut(), mock_env(), info.clone(), id);
 
         let vote2 = ExecuteMsg::Vote {
             proposal_id: id,
             vote: Vote::Yes,
         };
-        let info = mock_info(OWNER, &[]);
+        let info = mock_info(addr1.as_str(), &[]);
         execute(deps.as_mut(), mock_env(), info.clone(), vote2);
 
 
@@ -97,8 +111,9 @@ mod tests {
             proposal_id: id,
             vote: Vote::Yes,
         };
-        let info = mock_info(OWNER, &[]);
+        let info = mock_info(addr2.as_str(), &[]);
         execute(deps.as_mut(), mock_env(), info.clone(), vote3);
+
 
         prop.status =Status::Passed;
         PROPOSALS.save(&mut deps.storage, id, &prop);
@@ -107,31 +122,32 @@ mod tests {
             proposal_id: id 
         };
         execute(deps.as_mut(), mock_env(), info.clone(),execute_msg);
-        // let err = execute_execute(deps.as_mut(), mock_env(), info, id);
 
-
-
-
-
-
-
-
-
-        pub const VOTERDEPOSIT: Map<(u64, &Addr), Vec<Coin>> = Map::new("voter deposit");
-        let info = mock_info(OWNER, &[]);
-        let mut _vote = VOTERDEPOSIT.save(&mut deps.storage, (id, &info.sender), &info.funds);
-        let _deposit_info = VOTERDEPOSIT
-            .may_load(&deps.storage, (id, &info.sender))
-            .unwrap();
-        let err = execute_deposit(deps.as_mut(), mock_env(), info.clone(), id);
-
-        let info = mock_info(OWNER, &[]);
-        let vote2 = ExecuteMsg::Vote {
-            proposal_id: id,
-            vote: Vote::Yes,
-        };
-        execute(deps.as_mut(), mock_env(), info.clone(), vote2);
-
-     
         
+
+        
+        prop.status = Status::Passed;
+        let a = Uint128::from(123u128);
+        let deposit_info1 = Some(vec![Coin {
+            denom: "coin".to_string(),
+            amount: a,
+        }])
+        .unwrap();
+        let mut _vot = VOTERDEPOSIT.save(&mut deps.storage, (id, &info.sender), &deposit_info1);
+        _vot = PROPOSALS.save(&mut deps.storage, id, &prop);
+        let refund_msg = ExecuteMsg::Refund { 
+            proposal_id: id
+        };
+       let k = execute(deps.as_mut(), mock_env(), info.clone(),refund_msg);
+        assert_eq!(
+            k,
+            Ok(Response::new()
+                .add_message(BankMsg::Send {
+                    to_address: info.sender.to_string(),
+                    amount: deposit_info1,
+                })
+                .add_attribute("action", "refund")
+                .add_attribute("sender", info.sender)
+                .add_attribute("proposal_id", id.to_string()))
+        );
     }
