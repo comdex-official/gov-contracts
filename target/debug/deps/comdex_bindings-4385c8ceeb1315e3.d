f1cfbaf91d6ba31a083017aeaa0ebacc34f65736
Subham <39151404+Subham2804@users.noreply.github.com>
2022-07-01 17:08:43 +0530

    updated wasm

---
+++ target/debug/deps/comdex_bindings-4385c8ceeb1315e3.d
@@ -0,0 +1,7 @@
+/Users/subham/gov-contracts/target/debug/deps/comdex_bindings-4385c8ceeb1315e3.rmeta: packages/bindings/src/lib.rs packages/bindings/src/msg.rs packages/bindings/src/query.rs
+
+/Users/subham/gov-contracts/target/debug/deps/comdex_bindings-4385c8ceeb1315e3.d: packages/bindings/src/lib.rs packages/bindings/src/msg.rs packages/bindings/src/query.rs
+
+packages/bindings/src/lib.rs:
+packages/bindings/src/msg.rs:
+packages/bindings/src/query.rs:
---
+++ target/debug/deps/wasmer_middlewares-039a27afa5429083.d
@@ -0,0 +1,6 @@
+/Users/subham/gov-contracts/target/debug/deps/wasmer_middlewares-039a27afa5429083.rmeta: /Users/subham/.cargo/registry/src/github.com-1ecc6299db9ec823/wasmer-middlewares-2.2.1/src/lib.rs /Users/subham/.cargo/registry/src/github.com-1ecc6299db9ec823/wasmer-middlewares-2.2.1/src/metering.rs
+
+/Users/subham/gov-contracts/target/debug/deps/wasmer_middlewares-039a27afa5429083.d: /Users/subham/.cargo/registry/src/github.com-1ecc6299db9ec823/wasmer-middlewares-2.2.1/src/lib.rs /Users/subham/.cargo/registry/src/github.com-1ecc6299db9ec823/wasmer-middlewares-2.2.1/src/metering.rs
+
+/Users/subham/.cargo/registry/src/github.com-1ecc6299db9ec823/wasmer-middlewares-2.2.1/src/lib.rs:
+/Users/subham/.cargo/registry/src/github.com-1ecc6299db9ec823/wasmer-middlewares-2.2.1/src/metering.rs:
