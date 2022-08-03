5e289ce70e19db56b1d392d7c40304ed85b545d5
Coder-RG <36698583+Coder-RG@users.noreply.github.com>
2022-08-03 11:37:43 +0530

    The initial setup is complete
    All the necessary structs have been added, most important of which are
    TokenInfo and Vtoken

--- contracts/gov-locker/src/state.rs
+++ contracts/gov-locker/src/state.rs
@@ -1,24 +1,43 @@
-use crate::PeriodWeight;
-use cosmwasm_std::{Addr, Timestamp};
-use cw_storage_plus::{Item, Map, PrimaryKey};
+use cosmwasm_std::Addr;
+use cw_storage_plus::{Item, Map};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use std::time::{Duration, SystemTime};
+use std::time::SystemTime;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Copy, JsonSchema)]
-pub enum VestingPeriod {
-    SHORT,
-    MEDIUM,
-    LONG,
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Copy, JsonSchema)]
+// pub enum VestingPeriod {
+//     SHORT,
+//     MEDIUM,
+//     LONG,
+// }
+
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Copy, JsonSchema)]
+// pub struct LockingPeriod {
+//     pub _type: VestingPeriod,
+//     pub _time: SystemTime,
+//     pub _weight: i64,
+// }
+
+/// `period` is the locking period in **seconds** and `weight` is used to
+/// calculate the amount of tokens returned. For example, if the weight is 0.25
+/// and the deposited amount is 100DENOM, then 25uDENOM (100*0.25) is returned.
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct PeriodWeight {
+    period: u128,
+    weight: f64,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Copy, JsonSchema)]
-pub struct LockingPeriod {
-    pub _type: VestingPeriod,
-    pub _time: SystemTime,
-    pub _weight: i64,
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum LockingPeriod {
+    T1,
+    T2,
+    T3,
+    T4,
 }
 
+/// Holds the status of currently locked tokens. *Locked* means the tokens are
+/// in the vesting period and *Released* means the tokens have completed their
+/// vesting period and have been unlocked.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub enum Status {
     Locked,
@@ -26,19 +45,35 @@ pub enum Status {
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub struct Locked {
-    pub token: i64,
-    pub value: i64,
-    pub period: VestingPeriod,
+pub struct Vtoken {
+    /// amount of token being locked
+    pub token: u64,
+    /// amount of vtoken released
+    pub vtoken: u64,
+    /// Locking period i.e. T1..4
+    pub period: LockingPeriod,
+    /// Time at which the tokens were locked
     pub start_time: SystemTime,
+    /// Point in time after which the tokens can be unlocked
     pub end_time: SystemTime,
+    /// Current status of the tokens
     pub status: Status,
 }
 
-pub const LOCKED: Item<Locked> = Item::new("Locked");
-pub const VPERIOD: Map<i64, LockingPeriod> = Map::new("VestingPeriods");
-pub const STATE: Item<State> = Item::new("state");
+/// NFT struct for holding the token info
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct TokenInfo {
+    /// Owner of the NFT
+    owner: Addr,
+    /// Amount of vtokens issued
+    vtokens: Vec<Vtoken>,
+    token_id: u64,
+}
 
+// pub const LOCKED: Item<Locked> = Item::new("Locked");
+// pub const VPERIOD: Map<i64, LockingPeriod> = Map::new("VestingPeriods");
+
+/// Contains the four locking periods and the unlock period.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
     pub t1: PeriodWeight,
@@ -48,4 +83,5 @@ pub struct State {
     pub unlock_period: u128,
 }
 
-// pub const STATE: Item<State> = Item::new("state");
+pub const STATE: Item<State> = Item::new("state");
+pub const VTOKENS: Map<(Addr, u64), TokenInfo> = Map::new("vtokens");
